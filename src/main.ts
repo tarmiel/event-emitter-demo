8acfd1eb3337a86c6817e2b654019563f14759b3
Verhusha Oleg <olegverhusha@gmail.com>
2025-03-06 21:24:01 +0300

    feat: class example

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,135 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+type TodoItem = {
+  id: string;
+  text: string;
+  completed: boolean;
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+type Sort = {
+  text: "ask" | "desc" | "none";
+};
+
+class Store {
+  public todoList: TodoItem[] = [];
+  public sort: Sort = {
+    text: "none",
+  };
+
+  get sortedTodos() {
+    return [...this.todoList].sort((a, b) => {
+      switch (this.sort.text) {
+        case "ask":
+          return a.text.localeCompare(b.text);
+        case "desc":
+          return b.text.localeCompare(a.text);
+        default:
+          return 0;
+      }
+    });
+  }
+
+  addTodo(text: string) {
+    this.todoList.push({
+      id: crypto.randomUUID(),
+      text: text,
+      completed: false,
+    });
+
+    counterView.renderCounter();
+    todoView.renderTodoList();
+  }
+
+  removeTodo(id: string) {
+    this.todoList = this.todoList.filter((todo) => todo.id !== id);
+
+    counterView.renderCounter();
+    todoView.renderTodoList();
+  }
+
+  updateSort(sort: Sort) {
+    this.sort = sort;
+
+    counterView.renderCounter();
+    todoView.renderTodoList();
+  }
+}
+
+const store = new Store();
+
+class CounterView {
+  constructor() {
+    this.renderCounter();
+  }
+  renderCounter() {
+    const counterContainer = document.getElementById("counter")!;
+
+    counterContainer.innerHTML = `count is ${store.todoList.length}`;
+  }
+}
+const counterView = new CounterView();
+
+class TodoView {
+  private todoContainer: HTMLDivElement;
+
+  constructor() {
+    this.todoContainer = document.querySelector(".todo-list") as HTMLDivElement;
+    const input = this.todoContainer.querySelector(
+      "#todoInput"
+    ) as HTMLInputElement;
+    const button = this.todoContainer.querySelector(
+      "#addButton"
+    ) as HTMLButtonElement;
+
+    button.addEventListener("click", () => {
+      const text = input.value.trim();
+      if (text !== "") {
+        console.log(store, "store");
+        store.addTodo(text);
+        input.value = "";
+      }
+    });
+
+    this.todoContainer.addEventListener("click", (e) => {
+      if (
+        e.target instanceof HTMLButtonElement &&
+        e.target.classList.contains("deleteBtn")
+      ) {
+        const id = e.target.dataset.id!;
+        store.removeTodo(id);
+      }
+    });
+  }
+
+  renderTodoList() {
+    const todoListContainer = this.todoContainer.querySelector(".list")!;
+
+    const renderTodo = (todo: TodoItem) => `
+      <div>
+        <span>${todo.text}</span>
+        <button class="deleteBtn" data-id="${todo.id}">Delete</button>
+      </div>
+    `;
+
+    todoListContainer.innerHTML = store.sortedTodos.map(renderTodo).join("\n");
+  }
+}
+const todoView = new TodoView();
+
+class SortView {
+  private sortContainer: HTMLDivElement;
+
+  constructor() {
+    this.sortContainer = document.getElementById("sort") as HTMLDivElement;
+
+    this.sortContainer
+      .querySelector("#sortSelect")!
+      .addEventListener("change", (e) => {
+        console.log(e);
+        const text = (e.target as HTMLSelectElement).value as Sort["text"];
+        store.updateSort({ text: text });
+      });
+  }
+}
+
+new SortView();
